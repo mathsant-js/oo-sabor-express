6a6d1bdf1a2a3c40a56d1aa1e13f746380d53458
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-07-04 17:46:33 -0300

    feat: 2. Property
    - Aula 03: Property e métodos de classe

--- modelos/Restaurante.py
+++ modelos/Restaurante.py
@@ -1,18 +1,26 @@
 class Restaurante:
     restaurantes = []
     
+    # self._ativo -> estado interno do objeto (dado)
+    # ativo -> interface pública (como a variável será apresentada)
+    
     def __init__(self, nome, categoria):    
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
         return f"{self.nome} | {self.categoria}"
     
     def listar_restaurantes():
+        print(f"{'Nome do Restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status")
         for restaurante in Restaurante.restaurantes:
-            print(f"{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}")
+            print(f"- {restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo}")
+    
+    @property
+    def ativo(self):
+        return "✅" if self._ativo else "❌"
     
 restaurante_praca = Restaurante("Praça", "Italiana")
 restaurante_pizza = Restaurante("Pizza Place", "Fast Food")
