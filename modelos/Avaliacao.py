3a00243d6d6cbcba31acc5b86e06fb9e2138d4b5
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-07-06 14:11:15 -0300

    docs: adicionando docstrings nas classes Avaliacao e Restaurante

--- modelos/Avaliacao.py
+++ modelos/Avaliacao.py
@@ -1,12 +1,20 @@
 class Avaliacao:
+    """Representa uma avaliação de restaurante"""
     def __init__(self, cliente, nota):
+        """Inicializa uma instância de Avaliação
+        
+        Parâmetros:
+        - cliente (str): nome do cliente
+        - nota (float): nota do cliente atribuída ao restaurante"""
         self._cliente = cliente
         self._nota = nota
         
     @property
     def cliente(self):
+        """Exibição do nome do cliente"""
         return self._cliente
     
     @property
     def nota(self):
+        """Exibição da nota do cliente"""
         return self._nota
--- modelos/Restaurante.py
+++ modelos/Restaurante.py
@@ -1,13 +1,21 @@
 from modelos.Avaliacao import Avaliacao
 
 class Restaurante:
+    """Representa um restaurante e suas características"""
+    
     restaurantes = []
     
     # self._ativo -> estado interno do objeto (dado)
     # ativo -> interface pública (como a variável será apresentada)
     # String vazia em Python é considerada False em um contexto Booleano
     
-    def __init__(self, nome, categoria):    
+    def __init__(self, nome, categoria):
+        """Inicializa uma instância de Restaurante.
+        
+        Parâmetros:
+        - nome (str): nome do restaurante
+        - categoria (str): categoria do restaurante
+        """
         self._nome = nome.title()
         self._categoria = categoria
         self._ativo = False
@@ -15,10 +23,12 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
+        """Retorna uma representação em string do restaurante"""
         return f"{self.nome} | {self.categoria}"
     
     @classmethod
     def listar_restaurantes(cls):
+        """Exibe uma tabela formatada de todos os restaurantes"""
         print("-" * 79)
         print(f"| {'Nome do Restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | {'Avaliação'.ljust(20)} | Status |")
         print(f"|{'-' * 22}|{'-' * 22}|{'-' * 22}|{'-' * 8}|")
@@ -28,24 +38,35 @@ class Restaurante:
     
     @property
     def nome(self):
+        """Exibição do nome do restaurante"""
         return self._nome
     
     @property
     def avaliacoes(self):
+        """Exibição das avaliações do restaurante"""
         return self._avaliacoes
     
     @property
     def ativo(self):
+        """Retorna um símbolo do estado do restaurante para saber se está ativo ou não"""
         return "✅" if self._ativo else "❌"
     
     def alternar_estado(self):
+        """Alterna o estado para ativo ou desativado"""
         self._ativo = not self._ativo
         
     @staticmethod
     def avaliacao_valida(nota):
+        """Retorna se uma avaliação é válida"""
         return nota <= 5 and nota >= 0
         
     def receber_avaliacao(self, cliente, nota):
+        """Recebe avaliação e adiciona em uma lista de avaliações do restaurante
+        
+        Parâmetros:
+        - cliente (str): nome do cliente
+        - nota (float): nota do atribuída ao restaurante (entre 0 à 5)
+        """
         if not Restaurante.avaliacao_valida(nota):
             print(f"[ERRO] A nota deve ser de 0 à 5!\nNota recebida: {nota}")
             return
@@ -54,12 +75,14 @@ class Restaurante:
         self._avaliacoes.append(avaliacao)
         
     def listar_avaliacoes(self):
+        """Retorna uma lista da avaliações de um restaurante"""
         print(f"Avaliações do Restaurante {self.nome}")
         for avaliacao in self.avaliacoes:
             print(f"- Nome: {avaliacao.cliente} | Avaliação: {avaliacao.nota}")
             
     @property
     def media_avaliacoes(self):
+        """Exibe a média de avaliações de um restaurante"""
         if not self.avaliacoes:
             return "Sem avaliação"
         
